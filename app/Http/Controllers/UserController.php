620deb3d6e46c1dde31ea0f876a03964f870047c
Khajornsak2545 <khajornsak.s@ku.th>
2025-01-31 21:39:06 +0700

    add file

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return response()->json(User::all());
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json($user, 201);
+    }
+
+    public function show($id)
+    {
+        return response()->json(User::findOrFail($id));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->update($request->all());
+
+        return response()->json($user);
+    }
+
+    public function destroy($id)
+    {
+        User::destroy($id);
+        return response()->json(['message' => 'Deleted successfully']);
+    }
+}
